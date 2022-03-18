475734d7c5e7fb7cfdc19dd9830067b54e23fa92
Shoyayeb <sa.fine@outlook.com>
2022-03-18 17:14:09 +0600

    express with cors added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.PORT || 5000;
+
+
+
+app.get("/", (req, res) => {
+    res.send(`server running on ${port}`)
+});
+app.listen(port, () => {
+    console.log(`http://localhost:${port}`, port);
+})
